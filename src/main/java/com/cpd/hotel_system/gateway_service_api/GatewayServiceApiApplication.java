89adf2e4087f7a1e95169acd6037330196cb638a
RansikaChandupa <ransikachandupa@gmail.com>
2025-10-21 09:10:55 +0530

    Initial commit

---
+++ src/main/java/com/cpd/hotel_system/gateway_service_api/GatewayServiceApiApplication.java
@@ -0,0 +1,18 @@
+package com.cpd.hotel_system.gateway_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = {
+        "com.cpd.hotel_system.gateway_service_api",
+        "config"
+})
+@EnableDiscoveryClient
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cpd/hotel_system/gateway_service_api/security/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.cpd.hotel_system.gateway_service_api.security;
+
+public class SecurityConfiguration {
+}
